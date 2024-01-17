2079ec5e5872e1949a5a74d1de079a7a1deb006f
Nik <72612658+amp1re@users.noreply.github.com>
2024-01-17 14:23:27 +0300

    update configs

--- tinkoff_invest_bot/__init__.py
+++ tinkoff_invest_bot/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for tinkoff_invest_bot."""
 
 __author__ = """Nikita Toloknov"""
-__email__ = '4170407@gmail.com'
-__version__ = '0.1.0'
+__email__ = "4170407@gmail.com"
+__version__ = "0.1.0"
